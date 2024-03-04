9557a342f730617829a62f1456416f4aad68b80a
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-03-04 11:07:18 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Estos son los Arrays con los elementos que iran anidados.
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domi = [".com", ".es", ".io", ".net"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainList = document.getElementById("domainList"); //Aquí enlazamosla lista en el id del html
+
+  for (let k = 0; k < pronoun.length; k++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let i = 0; i < noun.length; i++) {
+        for (let m = 0; m < domi.length; m++) {
+          let domain = pronoun[k] + adj[j] + noun[i] + domi[m]; //construye todas las posibilidades de dominios con las palabras que hay en todos los arrays
+          let listItem = document.createElement("li"); // Crear un nuevo elemento de lista <li> y establecer su contenido como el dominio
+          listItem.textContent = domain;
+          domainList.appendChild(listItem); // Agregar el elemento de lista a la lista en el HTML. .appendChild es un método que agrega un nuevo elemento como hijo de otro elemento.
+        }
+      }
+    }
+  }
 };
