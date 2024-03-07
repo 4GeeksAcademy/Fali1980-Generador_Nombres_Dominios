a0226040fcecec4d6434b5d4f1ab39b8628345d6
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-03-07 17:51:01 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -4,23 +4,39 @@ import "./style.css";
 
 //Estos son los Arrays con los elementos que iran anidados.
 let pronoun = ["the", "our"];
-let adj = ["great", "big"];
+let adjetive = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let domi = [".com", ".es", ".io", ".net"];
-
+let domainExtension = [".com", ".es", ".io", ".net"];
+//////-----------------------------Código simplicado con forEach----------------------------
 window.onload = function() {
   let domainList = document.getElementById("domainList"); //Aquí enlazamosla lista en el id del html
-
-  for (let k = 0; k < pronoun.length; k++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let i = 0; i < noun.length; i++) {
-        for (let m = 0; m < domi.length; m++) {
-          let domain = pronoun[k] + adj[j] + noun[i] + domi[m]; //construye todas las posibilidades de dominios con las palabras que hay en todos los arrays
-          let listItem = document.createElement("li"); // Crear un nuevo elemento de lista <li> y establecer su contenido como el dominio
-          listItem.textContent = domain;
-          domainList.appendChild(listItem); // Agregar el elemento de lista a la lista en el HTML. .appendChild es un método que agrega un nuevo elemento como hijo de otro elemento.
-        }
-      }
-    }
-  }
+  pronoun.forEach(pronounItem => {
+    adjetive.forEach(adjetiveItem => {
+      noun.forEach(nounItem => {
+        domainExtension.forEach(domainExtensionItem => {
+          let completeDomainMade =
+            pronounItem + adjetiveItem + nounItem + domainExtensionItem;
+          let listItem = document.createElement("li");
+          listItem.textContent = completeDomainMade;
+          domainList.appendChild(listItem);
+        });
+      });
+    });
+  });
 };
+
+  ///////---------------------Código antiguo-----------------------------------------
+  // for (let k = 0; k < pronoun.length; k++) {
+  //   for (let j = 0; j < adjetive.length; j++) {
+  //     for (let i = 0; i < noun.length; i++) {
+  //       for (let m = 0; m < domainExtension.length; m++) {
+  //         let completeDomainMade =
+  //           pronoun[k] + adjetive[j] + noun[i] + domainExtension[m]; //construye todas las posibilidades de domnios con las palabras que hay en todos los arrays
+  //         let listItem = document.createElement("li"); // Crear un nuevo elemento de lista <li> y establecer su contenido como el domnio
+  //         listItem.textContent = completeDomainMade;
+  //         domainList.appendChild(listItem); // Agregar el elemento de lista a la lista en el HTML. .appendChild es un método que agrega un nuevo elemento como hijo de otro elemento.
+  //       }
+  //     }
+  //   }
+  // }
+
